5d23a6094fe940292542cfe9a12e01e8f3bb768d
nimrit-singh <nimritsingh377@gmail.com>
2025-05-31 23:23:15 +0530

    initial frontend setup

---
+++ frontend/src/app/api/home/route.ts
@@ -0,0 +1,18 @@
+export const POST = async(request:Request)=>{
+console.log("Post request",request);
+}
+
+export const GET = async(request:Request)=>{
+console.log("Get Request",request);
+
+}
+
+export const PUT = async(request:Request)=>{
+console.log("Put",request);
+
+}
+
+export const DELETE = async(request:Request)=>{
+console.log(request);
+
+}
---
+++ frontend/src/lib/config/config.ts
@@ -0,0 +1,5 @@
+
+export const ENV = {
+  BACKEND_URL: process.env.BACKEND_URL || 'http://localhost:8000',
+  IS_DEV: process.env.IS_DEV === 'true'
+};
